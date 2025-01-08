e421c8f56053676b5a1d88920ce9d2206593bf50
jatinthakur204 <sjatinthakur22@gmail.com>
2025-01-08 16:59:19 +0000

    code updated

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,16 @@
+let name ="jatin thakur"
+let age ="20"
+let isLoggedIn = false
+
+//number
+//bigint
+//string ""
+//boolean =>  true, false
+
+//null=> standalone value
+//undefined
+
+//symbol => used for uniqueness
+
+
+//object
