0610488f936af4aef9facc697a11c800778ce5a5
jatinthakur204 <sjatinthakur22@gmail.com>
2025-01-08 17:33:39 +0000

    conversions of datatypes

---
+++ 01_basic/03_conversions.js
@@ -0,0 +1,28 @@
+// let score =false
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+//"33" => 33
+
+//"33asd" => NaN
+
+//true => 1, false=> 0
+
+
+let isLoggedIn = 1   
+
+let booleanIsLockedIn = Boolean(isLoggedIn)
+console.log(booleanIsLockedIn)
+// here we have converted a number in boolean//
+
+
+let mynumber = 26
+ let stringmynumber = String(mynumber)
+ console.log(stringmynumber)
+ console.log(typeof stringmynumber)
