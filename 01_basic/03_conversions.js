e2d14d394242c5325bb648b4303fa289a128fef6
jatinthakur204 <sjatinthakur22@gmail.com>
2025-01-08 17:59:27 +0000

    conversions and operations

--- 01_basic/03_conversions.js
+++ 01_basic/03_conversions.js
@@ -15,14 +15,42 @@
 //true => 1, false=> 0
 
 
-let isLoggedIn = 1   
+//let isLoggedIn = 1   
 
-let booleanIsLockedIn = Boolean(isLoggedIn)
-console.log(booleanIsLockedIn)
+//let booleanIsLockedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLockedIn)
 // here we have converted a number in boolean//
 
 
-let mynumber = 26
- let stringmynumber = String(mynumber)
- console.log(stringmynumber)
- console.log(typeof stringmynumber)
+//let mynumber = 26
+ //let stringmynumber = String(mynumber)
+ //console.log(stringmynumber)
+ //console.log(typeof stringmynumber)
+
+
+ //let jatin = "name"
+
+ //let booleanjatin = Boolean(jatin)
+ //console.log(typeof booleanjatin)
+
+//************operations ************//
+
+let value = 3
+
+let negvalue = -value
+
+console.log(negvalue)
+
+
+let str1 = "jatin"
+
+let str2 = " thakur"
+
+let str3 = str1 + str2
+
+console.log (str3)
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
