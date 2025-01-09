a10ca1717510f0dcff31771060e6be0c310bb2c3
jatinthakur204 <sjatinthakur22@gmail.com>
2025-01-09 17:51:05 +0000

    updated

---
+++ 01_basic/05_datatypes_rewine.js
@@ -0,0 +1,16 @@
+let str1 = "jatin"
+
+let str2 = " thakur"
+
+let str3 = str1 + str2
+
+console.log (str3)
+
+let str4 = "kuch"
+
+let str5 = " bhi"
+
+let str6 = str4 + str5
+
+console.log (str6)
+
