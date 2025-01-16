2b292aa3ad88514f6008c0282a8433a74027c8b7
jatinthakur204 <sjatinthakur22@gmail.com>
2025-01-16 18:09:26 +0000

    code updated

---
+++ 02_basic/01_array.js
@@ -0,0 +1,42 @@
+//arrays
+
+
+const myarray = [1, 2, 3, 4, 5,]
+
+const myarray2 = new Array (1, 2, 3, 4)
+
+// console.log(myarray[0])
+
+// console.log (myarray2[3])
+
+//methods in arrray
+
+// myarray.push(6)
+
+// myarray.pop()
+
+// myarray.unshift(9)
+// myarray.shift()
+
+// console.log(myarray.includes(9))
+
+// const newarray = myarray.join()
+
+// console.log(myarray)
+
+// console.log(newarray)
+
+//slice adnd splice
+
+console.log("A", myarray)
+
+const mna1 = myarray.slice(1, 3)
+
+console.log(mna1)
+
+console.log("B", myarray)
+
+const mna2 = myarray.splice(1, 3)
+
+console.log(mna2)
+
